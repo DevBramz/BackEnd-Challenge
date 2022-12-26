de44ed43a4cc09960be7051adad441852c7f4990
Eugine <eugine@ibuqacapital.com>
2022-12-27 01:19:15 +0300

    added export mixin

--- backend_challenge/core/exporter.py
+++ backend_challenge/core/exporter.py
@@ -1,66 +1,109 @@
-class TitleSearchMixin:
-def get_queryset(self):
-# Fetch the queryset from the parent's get_queryset
-queryset = super().get_queryset()
-# Get the q GET parameter
-q = self.request.GET.get('q')
-if q:
-156
+import csv
+from django.http import HttpResponse
+from django.template.defaultfilters import slugify
+from rest_framework.response import Response
+from rest_framework import status
+
+# class TitleSearchMixin:
+# def get_queryset(self):
+# # Fetch the queryset from the parent's get_queryset
+# queryset = super().get_queryset()
+# # Get the q GET parameter
+# q = self.request.GET.get('q')
+# if q:
+# 156
 
 
-def get_data_for_export(self, request, queryset, *args, **kwargs):
-        export_form = kwargs.pop('export_form', None)
-        return self.choose_export_resource_class(export_form)\
-            (**self.get_export_resource_kwargs(request, *args, **kwargs))\
-            .export(queryset, *args, **kwargs)
+# def get_data_for_export(self, request, queryset, *args, **kwargs):
+#         export_form = kwargs.pop('export_form', None)
+#         return self.choose_export_resource_class(export_form)\
+#             (**self.get_export_resource_kwargs(request, *args, **kwargs))\
+#             .export(queryset, *args, **kwargs)
 
-    def get_export_filename(self, file_format):
-        date_str = now().strftime('%Y-%m-%d')
-        filename = "%s-%s.%s" % (self.model.__name__,
-                                 date_str,
-                                 file_format.get_extension())
-        return filename
+#     def get_export_filename(self, file_format):
+#         date_str = now().strftime('%Y-%m-%d')
+#         filename = "%s-%s.%s" % (self.model.__name__,
+#                                  date_str,
+#                                  file_format.get_extension())
+#         return filename
 
 
-class ExportViewMixin(BaseExportMixin):
-    form_class = ExportForm
-
-    def get_export_data(self, file_format, queryset, *args, **kwargs):
+class ExportViewMixin:
+    """
+    EXport orders mixin
+    """
+    qs = None
+    
+    def export(self,qs):
+        
         """
-        Returns file_format representation for given queryset.
+
+        Returns a JSON response, transforming 'context' to make the export data
+
         """
-        data = self.get_data_for_export(self.request, queryset, *args, **kwargs)
-        export_data = file_format.export_data(data)
-        return export_data
-# return a filtered queryset
-return queryset.filter(title__icontains=q)
-# No q is specified so we return queryset
-return queryset
-
-
-
-def export(qs, fields=None):
-    model = qs.model
-    response = HttpResponse(mimetype='text/csv')
-    response['Content-Disposition'] = 'attachment; filename=%s.csv' % slugify(model.__name__)
-    writer = csv.writer(response)
-    # Write headers to CSV file
-    if fields:
-        headers = fields
-    else:
-        headers = []
+        
+        model = qs.model
+        # date_str = now().strftime('%Y-%m-%d')
+        resp = HttpResponse(content_type="text/csv")
+        resp["Content-Disposition"] = "attachment; filename=%s.csv" % (
+            slugify(model.__name__)
+        )
+        writer = csv.writer(resp)
+        # Write headers to CSV file
+        headers = [
+            "code",
+            "category",
+        ]
+        # opts = queryset.model._meta
+        # field_names = [field.name for field in opts.fields]
         for field in model._meta.fields:
-            headers.append(field.name)
-    writer.writerow(headers)
-    # Write data to CSV file
-    for obj in qs:
-        row = []
-        for field in headers:
-            if field in headers:
-                val = getattr(obj, field)
-                if callable(val):
-                    val = val()
-                row.append(val)
-        writer.writerow(row)
-    # Return CSV file to browser as download
-    return response
+            if field.name not in ("added", "edited", "id", "code", "category"):
+                headers.append(field.name)
+
+        writer.writerow(headers)
+        # Write data to CSV file
+        for obj in qs:
+            row = []
+            for field in headers:
+                if field in headers:
+                    val = getattr(obj, field)
+                    if callable(val):
+                        val = val()
+                    row.append(val)
+            writer.writerow(row)
+        # Return CSV file to browser as download
+        if resp:
+
+            return resp
+        else:
+            return Response(
+                {"status": "failed", "message": "export failed"},
+                status=status.HTTP_410_GONE,
+            )
+
+
+# def export(qs, fields=None):
+#     model = qs.model
+#     response = HttpResponse(mimetype='text/csv')
+#     response['Content-Disposition'] = 'attachment; filename=%s.csv' % slugify(model.__name__)
+#     writer = csv.writer(response)
+#     # Write headers to CSV file
+#     if fields:
+#         headers = fields
+#     else:
+#         headers = []
+#         for field in model._meta.fields:
+#             headers.append(field.name)
+#     writer.writerow(headers)
+#     # Write data to CSV file
+#     for obj in qs:
+#         row = []
+#         for field in headers:
+#             if field in headers:
+#                 val = getattr(obj, field)
+#                 if callable(val):
+#                     val = val()
+#                 row.append(val)
+#         writer.writerow(row)
+#     # Return CSV file to browser as download
+#     return response
--- backend_challenge/core/views.py
+++ backend_challenge/core/views.py
@@ -1,38 +1,24 @@
-import json
-
-import requests
-from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
-from django.shortcuts import get_object_or_404, render
-from requests.auth import HTTPBasicAuth
-from rest_framework import status
+from rest_framework import status, viewsets, filters
+from django.db import transaction
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.exceptions import NotFound
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
-from .models import Customer, Order,generate_secret
-from .serializers import CustomerSerializer, OrderSerializer,CustomerProfileUnavailable
-from rest_framework import status, viewsets,filters
-
 from rest_framework.decorators import action
-from rest_framework.response import Response
+from .models import Customer, Order
+from .serializers import CustomerSerializer, OrderSerializer
 from .tasks import send_sms
-from django.db import transaction
-from django.http import Http404
-from django.core.exceptions import ObjectDoesNotExist
-from rest_framework.exceptions import APIException, NotFound, ValidationError
-from django_filters.rest_framework import DjangoFilterBackend
-import csv
-from django.http import HttpResponse, HttpResponseForbidden
-from django.template.defaultfilters import slugify
-from django.utils.timezone import now
+from .exporter import ExportViewMixin
+
 
 class CustomerProfileException(NotFound):
     """Exception raised when customer profile  is not present in the data."""
-    status_code = 404
-    default_detail = 'Customer Profile not found.Create Cussomer profile'
 
+    status_code = 404
+    default_detail = "Customer Profile not found.Create Cussomer profile"
 
 
 class Customer_Create(APIView):
@@ -54,18 +40,18 @@ class Customer_Create(APIView):
 
 
 class OrderListCreateAPIView(ListCreateAPIView):
-    
+
     """"""
-    
+
     serializer_class = OrderSerializer
     permission_classes = [IsAuthenticated]
     queryset = Order.objects.all()
 
     def get_queryset(self):
-    
-        user= self.request.user
+
+        user = self.request.user
         try:
-            customer= Customer.objects.get(user=user)
+            customer = Customer.objects.get(user=user)
         # if not customer:
         #     raise Http404("Create Customer Profile")
         except ObjectDoesNotExist as snip_no_exist:
@@ -75,42 +61,19 @@ class OrderListCreateAPIView(ListCreateAPIView):
     def perform_create(self, serializer):
         if serializer.is_valid():
             order = serializer.save()
-            transaction.on_commit(lambda:send_sms(order.id))
-            
-            
-            
-# class OrderDetailView(RetrieveUpdateDestroyAPIView):
-
-#     serializer_class = OrderDetailSerializer
-#     lookup_url_kwarg = '_id'
-
-#     def get_queryset(self):
-#         last_two_days = now() - timedelta(days=2)
-#         return Question.objects.filter(pub_date__gt=last_two_days)
-    
-    
-    
+            transaction.on_commit(lambda: send_sms(order.id))
 
 
-
-class OrderViewSet(viewsets.ModelViewSet):
+class OrderViewSet(viewsets.ModelViewSet,ExportViewMixin,):
     """
     A viewset that provides the standard actions
     """
-    queryset = Order.objects.all()
-    serializer_class =OrderSerializer
-    filter_backends = [DjangoFilterBackend,filters.SearchFilter]
-    filterset_fields = ['category', 'status']
-    search_fields = ['code','category', 'status']
-    
-    # def get_queryset(self):
-    #     """
-    #     Optionally restricts the returned purchases to a given user,
-    #     by filtering against a `username` query parameter in the URL.
-    #     """
-        
 
-    #     return self.queryset
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ["category", "status"]
+    search_fields = ["code", "category", "status"]
 
     @action(detail=True)
     def clone_order(self, request, pk=None):
@@ -118,57 +81,23 @@ class OrderViewSet(viewsets.ModelViewSet):
         Method to clone an order
         """
         order = self.get_object()
-        order.pk=None
-        
-        
-        
+        order.pk = None
 
         order.save()
-        
+
         if order:
-            return Response({'status': 'order created from clone'})
+            return Response({"status": "order created from clone"})
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
-        
+
     @action(detail=False)
-    def export(self,request):
+    def export_orders(self, request, *args, **kwargs):
+        """
+        Method to export orders
+        """
+        queryset = self.queryset
+        print(queryset)
+        return self.export(queryset)
         
-        qs=self.queryset
-        model = qs.model
-        # date_str = now().strftime('%Y-%m-%d')
-        resp = Response(content_type='text/csv')
-        resp['Content-Disposition'] = 'attachment; filename=%s.csv' % (slugify(model.__name__))
-        writer = csv.writer(resp)
-        # Write headers to CSV file
-        headers = ["code","category",]
-            # opts = queryset.model._meta
-            # field_names = [field.name for field in opts.fields]
-        for field in model._meta.fields:
-            if field.name not in ('added','edited','id','code','category'):
-                headers.append(field.name)
-       
-        
-        writer.writerow(headers)
-        # Write data to CSV file
-        for obj in qs:
-            row = []
-            for field in headers:
-                if field in headers:
-                    val = getattr(obj, field)
-                    if callable(val):
-                        val = val()
-                    row.append(val)
-            writer.writerow(row)
-        # Return CSV file to browser as download
-        if resp: 
-            
-            return resp
-        else:
-             return Response(
-                {'status': 'failed', 'message': 'export failed'},
-                status=status.HTTP_410_GONE
-            )
     
-            
-           
-           
+   
